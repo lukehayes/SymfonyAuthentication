b53dba5f16ea7b491665faec5f8726b3b01addcf
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-04 17:18:18 +0100

    Added RegistrationController to project.

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+use App\Entity\User;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/registration', name: 'app_registration')]
+    public function index(UserPasswordHasherInterface $passwordHasher, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        // Create a new user here for now.
+        $user = new User();
+        $user->setEmail("example@example.com");
+        $plainTextPassword = "password";
+
+
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plainTextPassword
+        );
+
+
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+
+        $entityManager->flush();
+
+
+        return $this->render('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+}
