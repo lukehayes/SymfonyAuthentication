777764c6e81a62e993f7e88557379701c9bda2a6
Luke Hayes <lukepurplehayes@gmail.com>
2022-10-09 11:37:15 +0100

    Added RegistrationController::show() for testing.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,6 +10,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RegistrationController extends AbstractController
 {
@@ -44,5 +45,16 @@ class RegistrationController extends AbstractController
         return $this->render('registration/index.html.twig', [
             'controller_name' => 'RegistrationController',
         ]);
+    }
+
+    /**
+     * Test method to dump registered users.
+     */
+    #[Route('/users', name: 'app_users')]
+    public function show(ManagerRegistry $doctrine) : Response
+    {
+        $users = $doctrine->getRepository(User::class)->find(1);
+        dump($users);
+        return new JsonResponse($users, 200, ["Content-Type" => "application/json"]);
     }
 }
